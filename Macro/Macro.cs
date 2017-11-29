3c79eeae4382c021a85e8574b03a5f709d370702
stefanocastello <steisbetter@me.com>
2017-11-29 10:26:22 +0100

    Add static MacroExpansion method

--- Macro/Macro.cs
+++ Macro/Macro.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Collections;
 
 namespace Macro
 {
     class Macro
     {
+        public static void MacroExpansion<T>() where T : IEnumerable
+        {
+            
+        }
     }
 }
