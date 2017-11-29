09d0bb24bbc1cf0fb0ce70746f469cd8e4276727
Stefano <steisbetter@me.com>
2017-11-29 10:09:31 +0100

    First Commit

---
+++ Lab5.Test/Macro.Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lab5.Test
+{
+    [TestFixture]
+    class Macro
+    {
+        [Test]
+        public void Tst()
+        {
+            Assert.That(true);
+        }
+    }
+}
