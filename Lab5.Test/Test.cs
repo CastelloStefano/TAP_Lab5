00199f5f90ba21b0de29f3f93975c7026b3a07a3
Stefano Castello <steisbetter@outlook.it>
2017-11-30 19:20:49 +0100

    First test complete

--- Lab5.Test/Test.cs
+++ Lab5.Test/Test.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Lab5.Test
@@ -7,12 +9,11 @@ namespace Lab5.Test
     [TestFixture]
     class Test
     {
-        [Test]
-        public void Atmp()
+        [TestCase(new[] { 1, 2, 1, 2, 3 }, 2, new[] { 7, 8, 9 }, new[] { 1, 7, 8, 9, 1, 7, 8, 9, 3 })]
+        [TestCase(new[] { "ciao","ste","hello","tele","ste","tele"}, "ste", new[] { "new", "main" }, new[] { "ciao", "new", "main", "hello", "tele", "new", "main", "tele" })]
+        public void Expansions<T>(IEnumerable<T> seq, T val, IEnumerable<T> newVal, IEnumerable<T> expect)
         {
-            IEnumerable<int> enumi = new List<int>(1);
-            int due = 2;
-            Assert.That(Macro.Macro.MacroExpansion<int>(enumi,due,enumi).GetType(),Is.EqualTo(1.GetType()));
+          Assert.That( Macro.Macro.MacroExpansion(seq, val,newVal),Is.EqualTo(expect));
         }
 
     }
--- Macro/Macro.cs
+++ Macro/Macro.cs
@@ -2,26 +2,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Macro
 {
     public class Macro
     {
-        public static T MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues)
+        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues)
         {
+            if(typeof(T) != value.GetType()) throw new InvalidEnumArgumentException();
             // Controllo dei tipi
-           // if()
-           // foreach (var x1 in newValues.ToList())
-              //  sequence.ToList().Insert(1,x1);
-
-            // ricerca
-
-            // sostituzione
-
-
-
-            return (T) value;
+            // if()
+            List<T> res = sequence.ToList();
+            List<T> NewValues = newValues.ToList();
+            List<T> resList = new List<T>();
+            foreach (var elem in res)
+            {
+                if (elem.Equals(value))
+                {
+                    foreach (var e in NewValues)
+                    {
+                       resList.Add(e);
+                    }
+                }
+                else
+                {
+                    resList.Add(elem);
+                }
+            }
+            return resList;
         }
     }
 }
