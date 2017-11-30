e183ba79d6490f4794d06e57bd5e3606b3813915
Stefano Castello <steisbetter@outlook.it>
2017-11-30 19:24:02 +0100

    refactored code

--- Macro/Macro.cs
+++ Macro/Macro.cs
@@ -12,16 +12,14 @@ namespace Macro
         public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues)
         {
             if(typeof(T) != value.GetType()) throw new InvalidEnumArgumentException();
-            // Controllo dei tipi
-            // if()
-            List<T> res = sequence.ToList();
-            List<T> NewValues = newValues.ToList();
-            List<T> resList = new List<T>();
+            var res = sequence.ToList();
+            var substitute = newValues.ToList();
+            var resList = new List<T>();
             foreach (var elem in res)
             {
                 if (elem.Equals(value))
                 {
-                    foreach (var e in NewValues)
+                    foreach (var e in substitute)
                     {
                        resList.Add(e);
                     }
