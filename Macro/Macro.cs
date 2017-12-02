de3ccf80c6752c0a360ba960599a80d7b7924850
Stefano Castello <steisbetter@outlook.it>
2017-12-02 12:54:26 +0100

    IEnumerator

--- Macro/Macro.cs
+++ Macro/Macro.cs
@@ -9,11 +9,42 @@ namespace Macro
 {
     public class Macro
     {
-        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues)
+        public static IEnumerator<T> MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues)
         {
-            if(typeof(T) != value.GetType()) throw new InvalidEnumArgumentException();
-            var res = sequence.ToList();
-            var substitute = newValues.ToList();
+            //var enumerable = sequence as T[] ?? sequence.ToArray();
+            IEnumerator<T> sequencEnumerator = sequence.GetEnumerator();
+            //var values = newValues as IList<T> ?? newValues.ToList();
+            IEnumerator<T> newValuesEnumerator = newValues.GetEnumerator();
+            if(!sequencEnumerator.MoveNext()) throw new InvalidEnumArgumentException(); // Mangia il primo ?
+
+            T current = sequencEnumerator.Current;
+            while (sequencEnumerator.MoveNext()){
+                if (current.Equals(value))
+                {
+                    while (newValuesEnumerator.MoveNext())
+                        yield return newValuesEnumerator.Current;
+                }
+                else
+                {
+                    yield return current;
+                }
+                current = sequencEnumerator.Current;
+            }
+            if (current.Equals(value))
+            {
+                while (newValuesEnumerator.MoveNext())
+                    yield return newValuesEnumerator.Current;
+            }
+            else
+            {
+                yield return current;
+            }
+
+            /*
+
+            if (typeof(T) != value.GetType()) throw new InvalidEnumArgumentException();
+            var res = newValuesEnumerator.ToList();
+            var substitute = values.ToList();
             var resList = new List<T>();
             foreach (var elem in res)
             {
@@ -29,7 +60,12 @@ namespace Macro
                     resList.Add(elem);
                 }
             }
-            return resList;
+            foreach (var rs in resList)
+            {
+                yield return rs;
+            }
+            //return resList;
+            */
         }
     }
 }
